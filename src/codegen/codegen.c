4e1e327ed26e4825d92f1509c919532d47396ab5
TeckWIT <tant1@wit.edu>
2026-05-11 13:14:00 -0400

    code gen ready

--- src/codegen/codegen.c
+++ src/codegen/codegen.c
@@ -1,1 +1,5 @@
 #include "codegen.h"
+
+void codegen_init(CodeGen *cg, IRList *ir, FILE *output) {}
+void codegen_emit(CodeGen *cg) {}
+void codegen_free(CodeGen *cg) {}
--- src/codegen/codegen.h
+++ src/codegen/codegen.h
@@ -1,3 +1,17 @@
 #ifndef CODEGEN_H
 #define CODEGEN_H
+
+#include "../ir/ir.h"
+#include <stdio.h>
+
+typedef struct {
+  IRList *ir;
+  FILE *output;
+  int stack_size;
+} CodeGen;
+
+void codegen_init(CodeGen *cg, IRList *ir, FILE *output);
+void codegen_emit(CodeGen *cg);
+void codegen_free(CodeGen *cg);
+
 #endif
