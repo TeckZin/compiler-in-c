4793f2d1d4239bd89f8d49b0119979084ff13042
TeckWIT <tant1@wit.edu>
2026-05-11 13:10:18 -0400

    irgen.c done

--- src/ir/irgen.c
+++ src/ir/irgen.c
@@ -1,7 +1,6 @@
 #include "irgen.h"
 #include "ir.h"
 
-#include <complex>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -35,12 +34,14 @@ void irgen_statement(IRGen *gen, ASTNode *node) {
 
 void irgen_var_decl(IRGen *gen, ASTNode *node) {
   int value = irgen_expression(gen, node->var_decl.value);
-  gen->reg_counter += value;
+  ir_list_add(&gen->list,
+              ir_new_instr(IR_MOV, -(node->var_decl.name[0]), value, 0));
 }
 
 void irgen_assign(IRGen *gen, ASTNode *node) {
   int value = irgen_expression(gen, node->assign.value);
-  gen->reg_counter += value;
+  ir_list_add(&gen->list,
+              ir_new_instr(IR_MOV, -(node->assign.name[0]), value, 0));
 }
 
 int irgen_expression(IRGen *gen, ASTNode *node) {
@@ -59,12 +60,42 @@ int irgen_expression(IRGen *gen, ASTNode *node) {
 }
 
 int irgen_binop(IRGen *gen, ASTNode *node) {
-  int value = 0;
-  value += irgen_expression(gen, node->binop.left);
-  value += irgen_expression(gen, node->binop.right);
-  return value;
+  int left = irgen_expression(gen, node->binop.left);
+  int right = irgen_expression(gen, node->binop.right);
+  int dest = gen->reg_counter++;
+  IROp op;
+  switch (node->binop.op) {
+  case TOKEN_PLUS:
+    op = IR_ADD;
+    break;
+  case TOKEN_MINUS:
+    op = IR_SUB;
+    break;
+  case TOKEN_DIVIDE:
+    op = IR_DIV;
+    break;
+  case TOKEN_MULTIPLY:
+    op = IR_MUL;
+    break;
+  default:
+    op = IR_ADD;
+    break;
+  }
+
+  ir_list_add(&gen->list, ir_new_instr(op, dest, left, right));
+
+  return dest;
 }
 
-int irgen_int_lit(IRGen *gen, ASTNode *node) { return 0; }
+int irgen_int_lit(IRGen *gen, ASTNode *node) {
+  int dest = gen->reg_counter++;
+  ir_list_add(&gen->list, ir_new_instr(IR_MOV, dest, node->int_lit.value, 0));
+  return dest;
+}
 
-int irgen_ident(IRGen *gen, ASTNode *node) { return 0; }
+int irgen_ident(IRGen *gen, ASTNode *node) {
+  int dest = gen->reg_counter++;
+  ir_list_add(&gen->list,
+              ir_new_instr(IR_MOV, dest, -(node->ident.name[0]), 0));
+  return dest;
+}
